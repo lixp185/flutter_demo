57534c2f27fc8486a4c6792f95897df4da9de3a6
lixp <651203303@qq.com>
2022-10-14 17:47:19 +0800

    更新动画类

--- lib/widgets/animated.dart
+++ lib/widgets/animated.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/widgets/paint_demo.dart';
 
 /// 动画
 class AnimateWidgetDemo extends StatefulWidget {
@@ -23,26 +22,28 @@ class _AnimateWidgetDemoState extends State<AnimateWidgetDemo>
   @override
   void initState() {
     super.initState();
-
     //vsync TickerProvider参数 创建Ticker 为了防止动画消耗不必要的资源
     _controller = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 2000)); //2s
+      vsync: this,
+      //设置Ticker 动画帧的回调函数
+      duration: const Duration(milliseconds: 2000),
+      // 正向动画时长 //2s
+      reverseDuration: const Duration(milliseconds: 2000),
+      // 反向动画时长 //2s
+      lowerBound: 0,
+      // 开始动画数值
+      upperBound: 1.0,
+      // 结束动画数值
+      animationBehavior: AnimationBehavior.normal,
+      // 动画器行为 是否重复动画 两个枚举值
+      debugLabel: "缩放动画", // 调试标签 toString时显示
+    );
 
-    cure = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+    cure = CurvedAnimation(parent: _controller, curve: MyCurve());
     // 在2s之内从0变到100采用cure的运动轨迹变化
     // Tween定义数据的起始和终点
-    animation = Tween(begin: 0.0, end: 1.0).animate(_controller)
-      ..addStatusListener((status) {
-        // dismissed	动画在起始点停止
-        // forward	动画正在正向执行
-        // reverse	动画正在反向执行
-        // completed	动画在终点停止
-        if (status == AnimationStatus.completed) {
-          _controller.reverse(); //反向执行 100-0
-        } else if (status == AnimationStatus.dismissed) {
-          _controller.forward(); //正向执行 0-100
-        }
-      });
+
+    animation = Tween(begin: 0.0, end: 1.0).animate(cure);
 
     animation2 = Tween<Offset>(begin: Offset(0.0, 0.0), end: Offset(1.0, 0.0))
         .animate(cure);
@@ -50,9 +51,23 @@ class _AnimateWidgetDemoState extends State<AnimateWidgetDemo>
 
     // pi 3.14
     animation4 = Tween(begin: 0.0, end: pi * 2).animate(cure);
+    _controller.addListener(() {});
+    _controller.addStatusListener((status) {
+      // dismissed	动画在起始点停止
+      // forward	动画正在正向执行
+      // reverse	动画正在反向执行
+      // completed	动画在终点停止
+      if (status == AnimationStatus.completed) {
+        _controller.reverse(); //反向执行 100-0
+      } else if (status == AnimationStatus.dismissed) {
+        _controller.forward(); //正向执行 0-100
+      }
+    });
 
     // 启动动画
-    _controller.forward();
+    // _controller.forward();//正向开始动画
+    // _controller.reverse();//反向开始动画
+    _controller.repeat(); // 无限循环开始动画
   }
 
   @override
@@ -64,57 +79,69 @@ class _AnimateWidgetDemoState extends State<AnimateWidgetDemo>
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Column(
       children: [
-        PaintDemo(),
-        Column(
-          children: [
-            // 平移
-            Text("平移"),
-            SlideTransitionLogo(
-              child: FlutterLogo(),
-              animation: animation2,
-            ),
+        // 平移
+        // Text("平移"),
+        // SlideTransitionLogo(
+        //   child: FlutterLogo(),
+        //   animation: animation2,
+        // ),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("旋转"),
-                RotationTransitionLogo(
-                  child: FlutterLogo(),
-                  animation: animation3,
-                ),
-                Text("渐变"),
-                FadeTransition(
-                  opacity: animation3,
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    child: FlutterLogo(),
-                  ),
-                ),
-              ],
-            ),
-            Text("3d旋转动画(变换组件实现)"),
-            ZAnimatedLogo(animation: animation4, child: FlutterLogo()),
-            Text("组合动画"),
-            ZhAnimatedLogo(
-              animation: animation2,
-              animation2: animation3,
-              child: RotationTransitionLogo(
-                child: FlutterLogo(),
-                animation: animation3,
-              ),
-            ),
-
-            Text("缩放"),
-            AnimatedLogo(
-              // 缩放
+        // Row(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: [
+        //     Text("旋转"),
+        //     RotationTransitionLogo(
+        //       child: FlutterLogo(),
+        //       animation: animation3,
+        //     ),
+        //     Text("渐变"),
+        //     FadeTransition(
+        //       opacity: animation3,
+        //       child: Container(
+        //         width: 100,
+        //         height: 100,
+        //         child: FlutterLogo(),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        // Text("3d旋转动画(变换组件实现)"),
+        // ZAnimatedLogo(animation: animation4, child: FlutterLogo()),
+        Text("缩放"),
+        Center(
+          child: AnimatedBuilder(
               child: FlutterLogo(),
               animation: animation,
-            ),
-          ],
+              builder: (context, child) {
+                return Container(
+                  width: 100 * animation.value,
+                  height: 100 * animation.value,
+                  child: child,
+                );
+              }),
         )
+
+        // AnimatedLogo(
+        //   // 缩放
+        //   child: Container(
+        //     width: 100,
+        //     height: 100,
+        //     child: FlutterLogo(),
+        //   ),
+        //   animation: _controller,
+        // ),
+
+        // Text("组合动画"),
+        // ZhAnimatedLogo(
+        //   animation: animation2,
+        //   animation2: animation3,
+        //   child: RotationTransitionLogo(
+        //     child: FlutterLogo(),
+        //     animation: animation3,
+        //   ),
+        // ),
       ],
     );
   }
@@ -129,16 +156,10 @@ class AnimatedLogo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-        child: child,
-        animation: animation,
-        builder: (context, child) {
-          return Container(
-            width: animation.value * 100,
-            height: animation.value * 100,
-            child: child,
-          );
-        });
+    return ScaleTransition(
+      child: child,
+      scale: animation,
+    );
   }
 }
 
@@ -239,9 +260,11 @@ class ZhAnimatedLogo extends StatelessWidget {
   }
 }
 
-// Animation对象是Flutter动画库中的一个核心类，它生成指导动画的值。
-// Animation对象知道动画的当前状态（例如，它是开始、停止还是向前或向后移动），但它不知道屏幕上显示的内容。
-// AnimationController管理Animation。
-// CurvedAnimation 将过程抽象为一个非线性曲线.
-// Tween在正在执行动画的对象所使用的数据范围之间生成值。例如，Tween可能会生成从红到蓝之间的色值，或者从0到255。
-// 使用Listeners和StatusListeners监听动画状态改变。
+class MyCurve extends Curve {
+  @override
+  double transformInternal(double x) {
+    // 自定义变化曲线
+    // y = x的立方。这里可以理解为定义方程 x可以理解为0-1的变化过程 y即是返回0-1的位置，无需关系中间是如何计算的。
+    return x * x * x;
+  }
+}
